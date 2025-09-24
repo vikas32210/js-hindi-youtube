ac3a9dcc09e4e5b7d89542074a335cc99771336e
vikas32210 <vikas.yadav.322005@gmail.com>
2025-09-24 12:10:10 +0000

    coversion

---
+++ 01_basic/01_conversionoperation.js
@@ -0,0 +1,16 @@
+let score= "33"
+console.log(typeof score);
+console.log(typeof(score));
+let valInnumber = Number(score);
+console.log(typeof(score));
+console.log(typeof(valInnumber));
+
+//"33"= 33
+//33abc=nan
+//true=1
+//false=0
+
+
+//1=true 0=false
+//""=false
+//"hitesh"=true
